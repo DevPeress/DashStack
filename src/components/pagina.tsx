d612873423779b4f0d1c91821682f79ce450ae37
Peres <fabricioperesdsantos@gmail.com>
2025-05-31 18:44:32 -0300

    Ajuste do componente página

--- src/components/pagina.tsx
+++ src/components/pagina.tsx
@@ -1,6 +1,6 @@
 export function Pagina({children}: Readonly<{children: React.ReactNode}>) {
     return (
-        <main className="flex absolute w-[85.417vw] h-[52.604vw] top-[3.646vw] left-[12.5vw] bg-[#1B2431] items-center justify-center">
+        <main className="flex absolute w-[87.5vw] h-[52.604vw] top-[3.646vw] left-[12.5vw] bg-[#1B2431] items-center justify-center">
             {children}
         </main>
     )
