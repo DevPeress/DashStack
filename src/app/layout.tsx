28cc16b1263196232797934f38822b15f9007fa0
Peres <fabricioperesdsantos@gmail.com>
2025-05-31 18:32:25 -0300

    Remoção dos dados no .css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
     <html lang="en">
-      <body
-        className="overflow-hidden"
-      >
+      <body className="overflow-hidden">
         {children}
       </body>
     </html>
