cb8a696c17e873d35e7171f6e5ffdb6cc844f9df
Peres <fabricioperesdsantos@gmail.com>
2025-06-01 13:34:34 -0300

    Responsividade pc e table do team

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -10,7 +10,7 @@ import { Contratar } from "./contratar";
 
 interface Team {
   nome: string,
-  cargo: string,
+  position: string,
   email: string,
   foto: string
 }
@@ -39,19 +39,19 @@ export default function Home() {
       <SideBar />
 
       <Pagina>
-        <h1 className="absolute left-[2.7vw] top-[2vw] font-bold text-[32px] text-[#FFFFFF]">Team</h1>
-        <button className="absolute right-[2.7vw] top-[2vw] w-[147px] h-[48px] bg-[#4379EE] font-bold text-[14px] text-[#FFFFFF] rounded-[6px] hover:scale-115" onClick={alterarTipo}>{tipo ? "Voltar" : "Novo membro"}</button>
+        <h1 className="absolute md:left-[2.7vw] lg:left-[2.7vw] md:top-[3vw] lg:top-[2vw] font-bold md:text-[3.2vw] lg:text-[1.667vw] text-[#FFFFFF] select-none">Team</h1>
+        <button className="absolute md:right-[2.7vw] lg:right-[2.7vw] md:top-[3vw] lg:top-[2vw] md:w-[12vw] lg:w-[7.656vw] md:h-[4vw] lg:h-[2.5vw] bg-[#4379EE] font-bold md:text-[1.4vw] lg:text-[0.729vw] text-[#FFFFFF] rounded-[0.313vw] hover:scale-115 select-none" onClick={alterarTipo}>{tipo ? "Voltar" : "Novo membro"}</button>
         {tipo ? 
           <>
             <Contratar />
           </>
 
           :
-          <div className="flex absolute top-[6vw] w-[82vw] max-h-[37.5vw] h-auto items-center justify-left overflow-x-hidden overflow-y-auto flex-wrap">
+          <div className="grid md:grid-cols-2 lg:grid-cols-5 absolute top-[10vw] lg:top-[6vw] md:w-[82vw] lg:w-[82vw] md:max-h-[90vw] lg:max-h-[37.5vw] h-auto items-center justify-left overflow-x-hidden overflow-y-auto">
             {funcionarios.length > 0 && funcionarios.map((item,index) => {
 
               return(
-                <div key={index} className="flex relative w-[262px] h-[289px] mr-[2vw] mb-[1vw]">
+                <div key={index} className="flex relative md:w-[35vw] lg:w-[13.646vw] md:h-[40vw] lg:h-[15.052vw] mr-[2vw] mb-[1vw]">
                   <Image
                     src={'/Cards.svg'}
                     alt="Estrela do To-DO"
@@ -62,16 +62,16 @@ export default function Home() {
 
                   <div className="flex absolute w-full h-full items-center justify-center">
                     <Image
-                      className="absolute top-[2vw]"
+                      className="absolute md:top-[5vw] lg:top-[1vw]"
                       src={item.foto}
                       alt="Foto do usuário"
                       width={110}
                       height={111}
                       priority                
                     />
-                    <h1 className="absolute top-[9vw] text-[#FFFFFF] text-[16px] font-bold">{item.nome}</h1>
-                    <h2 className="absolute top-[10.5vw] text-[#FFFFFF] text-[14px] font-semibold opacity-[70%]">{item.position}</h2>
-                    <h2 className="absolute top-[12vw] text-[#FFFFFF] text-[14px] font-normal opacity-[80%]">{item.email}</h2>
+                    <h1 className="absolute md:top-[21.5vw] lg:top-[8vw] text-[#FFFFFF] md:text-[2vw] lg:text-[0.833vw] font-bold">{item.nome}</h1>
+                    <h2 className="absolute md:top-[25vw] lg:top-[9.5vw] text-[#FFFFFF] md:text-[1.6vw] lg:text-[0.729vw] font-semibold opacity-[70%]">{item.position}</h2>
+                    <h2 className="absolute md:top-[28vw] lg:top-[11vw] text-[#FFFFFF] md:text-[1.6vw] lg:text-[0.729vw] font-normal opacity-[80%]">{item.email}</h2>
                   </div>
                 </div>
               )
