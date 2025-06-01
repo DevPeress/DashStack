2094f48272a2fb9681c031526a50bfb650f8c807
Peres <fabricioperesdsantos@gmail.com>
2025-06-01 12:56:35 -0300

    Reponsividade da pagina

--- src/components/pagina.tsx
+++ src/components/pagina.tsx
@@ -1,6 +1,6 @@
 export function Pagina({children}: Readonly<{children: React.ReactNode}>) {
     return (
-        <main className="flex absolute w-[87.5vw] h-[52.604vw] top-[3.646vw] left-[12.5vw] bg-[#1B2431] items-center justify-center">
+        <main className="flex absolute w-[87.5vw] md:h-[105.3vw] lg:h-[52.604vw] top-[3.646vw] left-[12.5vw] bg-[#1B2431] items-center justify-center">
             {children}
         </main>
     )
